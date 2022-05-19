8fafea7e96e285cd51afb90327a7424a64f50a0a
Anthony Ivan <tonivanchis@gmail.com>
2022-05-18 23:42:13 -0500

    preview link

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,19 @@ export default function Home() {
           integrity="sha384-0fdwu/T/EQMsQlrHCCHoH10pkPLlKA1jL5dFyUOvB3lfeT2540/2g6YgSi2BL14p"
           crossOrigin="anonymous"
         />
+        <meta property="og:url" content="cardsmemo.com" />
+        <meta property="og:type" content="website" />
+        <meta property="fb:app_id" content="your fb app id" />
+        <meta
+          property="og:title"
+          content="Cardsmemo || La red social de información académica"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          property="og:description"
+          content="Cardsmemo te permite compartir publicaciones y flashcards con los demás usuarios de la red. Regístrate y empieza a crear y compartir tu propia red de conocimiento."
+        />
+        <meta property="og:image" content="https://res.cloudinary.com/cardsmemo/image/upload/v1652935086/app_src/ioo_pacwki.png" />
       </Head>
       <Navbar title="Inicio" />
       <Content content={content} />
