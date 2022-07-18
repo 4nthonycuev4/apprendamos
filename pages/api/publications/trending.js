5009b6133507f8c5e20988b8e8d3087701e711eb
Anthony Ivan <83598208+4nthony1van@users.noreply.github.com>
2022-07-18 17:51:00 -0500

    test1

--- pages/api/publications/trending.js
+++ pages/api/publications/trending.js
@@ -3,14 +3,14 @@ import FaunaClient from "../../../fauna";
 const TrendingPublications = async (req, res) => {
     try {
         const client = new FaunaClient();
-        const afterId = req.query && req.query.afterId
+        const afterId = req.query && req.query.afterId;
 
         const publications = await client.getTrendingPublications(afterId);
 
-        return res.status(200).json(publications);
+        res.status(200).json(publications);
     } catch (error) {
         console.log("error", error);
-        res.status(error.status || 500).json(error);
+        res.status(error.status || 500).json({ error });
     }
 }
 
